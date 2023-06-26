3f58940bce4a8be748299f78ad4c8ae7423f9e8b
Irsyad Nafi <121683533+IrsyadNafiA@users.noreply.github.com>
2023-06-26 11:49:39 +0700

    card kasaran

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../Utilities/Header";
 import CardLocation from "../Utilities/CardLocation";
+import Button from "../Utilities/Button";
 
 function Home() {
   return (
@@ -8,6 +9,10 @@ function Home() {
       <div className="w-full flex flex-col items-center justify-center">
         <Header title="Arrival Location" />
         <CardLocation title="International Ferry Terminal, Batam Center" />
+        <div className="bg-black/40 h-[100px] w-[250px] flex flex-col text-white gap-2 justify-center items-center rounded-xl">
+          <h3>Welcome To Batam</h3>
+          <Button link="#" title="See More" class="bg-white text-black hover:bg-black hover:text-white" />
+        </div>
       </div>
     </React.Fragment>
   );
--- src/components/Utilities/CardLocation.jsx
+++ src/components/Utilities/CardLocation.jsx
@@ -2,7 +2,7 @@ import Button from "./Button";
 
 function CardLocation(props) {
   return (
-    <div className="flex bg-white w-full h-10 items-center justify-between px-10">
+    <div className="flex bg-white w-full h-10 items-center justify-between px-10 py-10">
       <img src="" alt="ini gambar" />
       <p className="text-lg font-extrabold">{props.title}</p>
       <Button title="See More" class="hover:bg-black/80" link="#" />
